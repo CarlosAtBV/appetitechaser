5c711fc22a49e8ad201e6e3d164bff0a09b4ceae
DrPyspy <legowego88@gmail.com>
2021-08-20 17:22:02 -0600

    nikki mod swap anim wip

--- Blender Files/Weapons/DarlingNikki/render_nikki_composite.py
+++ Blender Files/Weapons/DarlingNikki/render_nikki_composite.py
@@ -4,27 +4,46 @@ import render_fetus2021
 
 scene = bpy.context.scene
 
+mods = [
+	{
+		"number":"1",
+		"path":"//../../../TSP PK3/sprites/weapons/mel/Darling Nikki/",
+		"visual":"GunStrip_Normal"
+	},
+	{
+		"number":"2",
+		"path":"//../../../TSP PK3/sprites/weapons/mel/Darling Nikki/Spitfire/",
+		"visual":"GunStrip_Spitfire"
+	}
+]
+
 frame_names = [
-	"121GA0",
-	"121GB0",
-	"121GC0",
-	"121GD0",
-	"121GE0",
+	"12{mod}GA0",
+	"12{mod}GB0",
+	"12{mod}GC0",
+	"12{mod}GD0",
+	"12{mod}GE0",
 	
-	"121RA0",
-	"121RB0",
-	"121RC0",
+	"12{mod}RA0",
+	"12{mod}RB0",
+	"12{mod}RC0",
+	
+	"12{mod}SA0",
 	
 	"120HA0",
 	"120HB0",
 	"120HC0",
 ];
 
-render_fetus2021.set_offsets((159, 50+32));
-render_fetus2021.set_path("//../../../TSP PK3/sprites/weapons/mel/Darling Nikki/");
+for mod in mods:
+	render_fetus2021.set_offsets((159, 50+32));
+	render_fetus2021.set_path(mod["path"]);
+	
+	for i in mods:
+		scene.sequence_editor.sequences_all[i["visual"]].mute = not (i == mod);
 
-for cnt, frame in enumerate(frame_names):
-	scene.timeline_markers.new(frame, frame=cnt);
+	for cnt, frame in enumerate(frame_names):
+		scene.timeline_markers.new(frame.format(mod = mod["number"]), frame=cnt);
 
-render_fetus2021.refresh_markers();
-render_fetus2021.render_frames_by_markers("POSS", "finalrender");
+	render_fetus2021.refresh_markers();
+	render_fetus2021.render_frames_by_markers("POSS", "finalrender");
