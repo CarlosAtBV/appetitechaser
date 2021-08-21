082681f04efca39eecee76f55e1f461b16feb05e
DrPyspy <legowego88@gmail.com>
2021-08-20 20:55:33 -0600

    super exagerrated spitfire mod toggle anim

--- Blender Files/Weapons/DarlingNikki/render_nikki_composite.py
+++ Blender Files/Weapons/DarlingNikki/render_nikki_composite.py
@@ -18,33 +18,39 @@ mods = [
 ]
 
 frame_names = [
-	"12{mod}GA0",
-	"12{mod}GB0",
-	"12{mod}GC0",
-	"12{mod}GD0",
-	"12{mod}GE0",
+	["12{mod}GA0"] * 2,
+	["12{mod}GB0"] * 2,
+	["12{mod}GC0"] * 2,
+	["12{mod}GD0"] * 2,
+	["12{mod}GE0"] * 2,
 	
-	"12{mod}RA0",
-	"12{mod}RB0",
-	"12{mod}RC0",
+	["12{mod}RA0"] * 2,
+	["12{mod}RB0"] * 2,
+	["12{mod}RC0"] * 2,
 	
-	"12{mod}SA0",
-	"12{mod}SB0",
+	["12{mod}SA0"] * 2,
+	["12{mod}SB0"]  * 2,
 	
-	"120HA0",
-	"120HB0",
-	"120HC0",
+	["120HA0", "ignore"],
+	["120HB0", "ignore"],
+	["120HC0", "ignore"],
+	
+	["ignore", "12{mod}AA0"],
+	["ignore", "12{mod}AB0"],
 ];
 
-for mod in mods:
+for cntmod, mod in enumerate(mods):
 	render_fetus2021.set_offsets((159, 50+32));
 	render_fetus2021.set_path(mod["path"]);
 	
 	for i in mods:
 		scene.sequence_editor.sequences_all[i["visual"]].mute = not (i == mod);
 
+	for m in scene.timeline_markers:
+		scene.timeline_markers.remove(m);
+	
 	for cnt, frame in enumerate(frame_names):
-		scene.timeline_markers.new(frame.format(mod = mod["number"]), frame=cnt);
+		scene.timeline_markers.new(frame[cntmod].format(mod = mod["number"]), frame=cnt);
 
 	render_fetus2021.refresh_markers();
 	render_fetus2021.render_frames_by_markers("POSS", "finalrender");
