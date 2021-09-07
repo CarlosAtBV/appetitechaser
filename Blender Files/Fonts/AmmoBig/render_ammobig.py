2b229eb1a9705b07c402980346e59d79d3252192
DrPyspy <legowego88@gmail.com>
2021-09-07 17:37:55 -0600

    new small ammo font + big ammo font adjustmenets

--- Blender Files/Fonts/AmmoBig/render_ammobig.py
+++ Blender Files/Fonts/AmmoBig/render_ammobig.py
@@ -3,23 +3,29 @@ import subprocess, math
 sys.path.append('../../RenderPipeline/');
 import render_crop2021
 
-curScene = bpy.context.scene
-basePath = '//../../../TSP PK3/fonts/tsp_ammo_big/'
+scene = bpy.context.scene
 
-for curFrame in range(48, 57+1):
-	getCode = format(curFrame, 'x');
+def renderFont(basePath):
+	for curFrame in range(48, 57+1):
+		getCode = format(curFrame, 'x');
 
-	frameName = format(getCode, "0>4");
+		frameName = format(getCode, "0>4");
 
-	bpy.data.objects["Text"].data.body = chr(curFrame);
-	
-	curScene.render.filepath = basePath+frameName;
-	bpy.ops.render.render(write_still = True);
-	origImg = bpy.data.images.load(bpy.context.scene.render.filepath+str('.png'));
-	cropImg, pX, pY = render_crop2021.crop_image_to_offset(origImg, 0, 0);
+		bpy.data.objects["Text"].data.body = chr(curFrame);
+		
+		scene.render.filepath = basePath+frameName;
+		bpy.ops.render.render(write_still = True);
+		origImg = bpy.data.images.load(bpy.context.scene.render.filepath+str('.png'));
+		cropImg, pX, pY = render_crop2021.crop_image_to_offset(origImg, 0, 0);
 
-	cropImg.file_format = 'PNG';
-	cropImg.filepath_raw = curScene.render.filepath+str('.png');
-	cropImg.save();
+		cropImg.file_format = 'PNG';
+		cropImg.filepath_raw = scene.render.filepath+str('.png');
+		cropImg.save();
 
-	process = subprocess.Popen([bpy.path.abspath('//../../RenderPipeline/compresssprite.bat'), str(0), str(pY), bpy.path.abspath(cropImg.filepath_raw)]);
+		process = subprocess.Popen([bpy.path.abspath('//../../RenderPipeline/compresssprite.bat'), str(pX), str(pY), bpy.path.abspath(cropImg.filepath_raw)]);
+		
+renderFont('//../../../TSP PK3/fonts/tsp_ammo_big/');
+
+bpy.data.objects["Text"].data.size *= 0.5;
+
+renderFont('//../../../TSP PK3/fonts/tsp_ammo_small/');
