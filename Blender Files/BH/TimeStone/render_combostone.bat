fecfe648796b6a70571bd6312de91b3334f847a0
DrPyspy <legowego88@gmail.com>
2021-10-21 11:30:15 -0600

    combo creestals ....

---
+++ Blender Files/BH/TimeStone/render_combostone.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Blender Foundation\Blender\blender" -b "ComboStone.blend" -P "render_combostone.py"
+timeout 5
---
+++ Blender Files/BH/TimeStone/render_combostone.py
@@ -0,0 +1,31 @@
+import sys, bpy
+sys.path.append('../../RenderPipeline/');
+import render_fetus2021
+
+scene = bpy.context.scene
+
+render_fetus2021.set_offsets((32, 72));
+render_fetus2021.set_path("//../../../TSP PK3/sprites/bh/combostone/");
+
+scene.layers = [l in [0] for l in range(20)];
+
+for m in scene.timeline_markers:
+	scene.timeline_markers.remove(m);
+
+for curFrame in range(scene.frame_start, scene.frame_end+1):
+	scene.timeline_markers.new("BHCS"+chr(65 + curFrame)+"0", frame=curFrame);
+
+render_fetus2021.refresh_markers();
+render_fetus2021.render_frames_by_markers("POSS", "finalrender");
+
+scene.layers = [l in [1] for l in range(20)];
+
+for m in scene.timeline_markers:
+	scene.timeline_markers.remove(m);
+
+for curFrame in range(scene.frame_start, scene.frame_end+1):
+	scene.timeline_markers.new("BHCF"+chr(65 + curFrame)+"0", frame=curFrame);
+
+render_fetus2021.set_offsets((32, 32));
+render_fetus2021.refresh_markers();
+render_fetus2021.render_frames_by_markers("POSS", "finalrender");
