d80b340667eeadc100edab98b7a062b7fdb4fd06
DrPyspy <legowego88@gmail.com>
2022-04-03 23:14:37 -0600

    add a handler that waits for monsters sprites to be initialzied before siccing them on the player

--- pepydir_launch_bh_chell.bat
+++ pepydir_launch_bh_chell.bat
@@ -1,1 +1,1 @@
-C:\Users\legow\Desktop\Games\devgz\gzdoom.exe -iwad DOOM2.wad -file "D:\Projects\TheSpacePirate\TSP Git\TSP PK3" "D:\Downloads\Games\Doom\GameplayMods\LOB_v1.05.pk3" +tsp_debug 1 -nomusic +tsp_gamemode 1 +map TSP_BH01
+C:\Users\legow\Desktop\Games\devgz\gzdoom.exe -iwad DOOM2.wad -file "D:\Projects\TheSpacePirate\TSP Git\TSP PK3" "D:\Downloads\Games\Doom\GameplayMods\Bratwurst_RecookedV0.9.3.pk3" +tsp_debug 1 -nomusic +tsp_gamemode 1 +map TSP_BH01
