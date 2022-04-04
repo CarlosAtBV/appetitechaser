6a8e69351939385904ffbad8a483d31b48499965
DrPyspy <legowego88@gmail.com>
2022-04-03 21:58:47 -0600

    Create pepydir_launch_bh_chell.bat

---
+++ pepydir_launch_bh_chell.bat
@@ -0,0 +1,1 @@
+C:\Users\legow\Desktop\Games\devgz\gzdoom.exe -iwad DOOM2.wad -file "D:\Projects\TheSpacePirate\TSP Git\TSP PK3" "D:\Downloads\Games\Doom\GameplayMods\Malefactors_v1.2.pk3" +tsp_debug 1 -nomusic +tsp_gamemode 1 +map TSP_BH01
