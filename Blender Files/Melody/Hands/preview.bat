f6484ae7bf246fa031f3bb006fc3039ea29a69f3
DrPyspy <legowego88@gmail.com>
2024-06-19 20:17:06 -0600

    more handiness

--- Blender Files/Melody/Hands/preview.bat
+++ Blender Files/Melody/Hands/preview.bat
@@ -1,7 +1,15 @@
-magick preview.png preview2.png -trim -append ^
-	-write MPR:orig -alpha off +dither -remap pal.png ^
+::magick preview.png preview2.png -trim -append ^
+::	-write MPR:orig -alpha off +dither -remap pal.png ^
+::	( MPR:orig -alpha extract -fx round(u) ) ^
+::	-compose CopyOpacity -composite ^
+::	preview3.png
+
+magick ^
+	( preview/twohands.png preview/twohands2.png -trim -append ) ^
+	-write MPR:orig -alpha off +dither -remap pal2.png ^
 	( MPR:orig -alpha extract -fx round(u) ) ^
 	-compose CopyOpacity -composite ^
-	preview3.png
+	-filter point -resize 200%% ^
+	preview/preview.png
 
 timeout 5
