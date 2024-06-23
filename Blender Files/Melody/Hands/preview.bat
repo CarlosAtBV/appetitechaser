b6b578770332add84df70dd7a2dcbc1fdd0c1063
DrPyspy <legowego88@gmail.com>
2024-06-22 18:35:47 -0600

    mel tendon technology... also more hand updates

--- Blender Files/Melody/Hands/preview.bat
+++ Blender Files/Melody/Hands/preview.bat
@@ -4,9 +4,15 @@
 ::	-compose CopyOpacity -composite ^
 ::	preview3.png
 
+magick pal_base.png ^
+	( -size 1x1 xc:#493437 xc:#854F3C xc:#FEE5C3 xc:#FFEEDA +append -resize 64x1^! ) ^
+	( -size 1x1 xc:#0A010A xc:#13050B xc:#353030 xc:#4E413F xc:#705445 +append -resize 24x1^! ) ^
+	+append ^
+	pal3.png
+
 magick -background none -gravity center ^
-	( preview/twohands.png preview/twohands2.png preview/twohands3.png -trim -append ) ^
-	-write MPR:orig -alpha off +dither -remap pal2.png ^
+	( rendertest/*.png -trim -append ) ^
+	-write MPR:orig -alpha off +dither -remap pal3.png ^
 	( MPR:orig -alpha extract -fx round(u) ) ^
 	-compose CopyOpacity -composite ^
 	-filter point -resize 200%% ^
