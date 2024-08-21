9fbd2fc7abd803ffb3177ec7e8795daeb4becf8c
DrPyspy <legowego88@gmail.com>
2024-08-21 13:14:39 -0600

    mel's silly fingorees begin to believe and become raelity

--- Blender Files/Melody/Hands/comp.bat
+++ Blender Files/Melody/Hands/comp.bat
@@ -19,4 +19,18 @@ magick -background none ^
 	( -gravity East render/melee_idle-3.png -extent 396x400 -extent 800x400 ) ^
 	comp/melee_idle-3.png
 	
+::cd comp
+
+FOR /R %%A IN (comp/*.png) DO (
+	echo comp/%%~nA
+	echo comp/%%~nA
+	magick comp/%%~nA.png ^
+		-write MPR:orig -alpha off +dither -remap pal3.png ^
+		MPR:orig -compose CopyOpacity -composite ^
+		-channel A -threshold 50%% +channel ^
+		comp/%%~nA.png
+)
+
+
+	
 timeout 5
---
+++ Blender Files/Melody/Hands/spriteify.py
@@ -0,0 +1,32 @@
+import os, subprocess
+from PIL import Image, ImageDraw
+
+myPath = (os.path.dirname(os.path.realpath(__file__)))+"/"
+gamePath = myPath+"../../../TSP PK3/sprites/"
+
+yOffset = -32
+
+pics = [
+	[ "comp/melee_idle-1-0.png", "weapons/mel/Bare Knuckle/111GA0.png" ],
+	[ "comp/melee_idle-2-0.png", "weapons/mel/Bare Knuckle/111GB0.png" ],
+	[ "comp/melee_idle-3-0.png", "weapons/mel/Bare Knuckle/111GC0.png" ],
+	[ "comp/melee_idle-1-1.png", "weapons/mel/Bare Knuckle/111GD0.png" ],
+	[ "comp/melee_idle-2-1.png", "weapons/mel/Bare Knuckle/111GE0.png" ],
+	[ "comp/melee_idle-3-1.png", "weapons/mel/Bare Knuckle/111GF0.png" ],
+]
+
+for picPair in pics:
+	pic = picPair[0]
+	path = picPair[1]
+	toConvert = Image.open(myPath+pic)
+	
+	daBox = toConvert.getbbox()
+	daSize = toConvert.size
+	toConvert = toConvert.crop(daBox)
+	offX = -(daBox[0] - daSize[0]) - (daSize[0]/2) - 160
+	offY = -(daBox[1] - daSize[1]) - (daSize[1]/2) - 100 + 32
+	
+	picPath = gamePath+path
+	print(picPath)
+	toConvert.save(picPath)
+	process = subprocess.Popen(['D:/Projects/RenderPipeline/grabpng.exe', '-grab', str(offX), str(offY), picPath]);
