a7d3794d2683e16fd970d05b0a40ac53d53d8ffa
DrPyspy <legowego88@gmail.com>
2026-06-11 20:14:17 -0600

    Update spriteify script to automatically offset larger sprites

--- tools/spriteify.py
+++ tools/spriteify.py
@@ -52,8 +52,11 @@ for command in commands:
 		path_out = f"{path_game}/{command_words[1].replace('"', '')}"
 		# 20
 		# 41
+		x_off = (cur_image.width/2)-160
+		y_off = ((cur_image.height/2)-120) + 61
 		path_dest = (os.path.dirname(os.path.realpath(path_file)))
 		os.makedirs(path_dest, exist_ok=True)
 		cur_image.save(filename=path_out)
-		subprocess.Popen(['D:/Projects/RenderPipeline/grabpng.exe', '-grab', str(0), str(61), path_out])
+		#subprocess.Popen(['D:/Projects/RenderPipeline/grabpng.exe', '-grab', str(0), str(61), path_out])
+		subprocess.Popen(['D:/Projects/RenderPipeline/grabpng.exe', '-grab', str(x_off), str(y_off), path_out])
 	#print(command)
